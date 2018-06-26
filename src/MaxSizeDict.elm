07ab849b3dd88754459addb5433ab3efe289736a
Michel Rijnders <michel@driebit.nl>
2018-06-26 14:20:44 +0200

    Add docs

--- src/MaxSizeDict.elm
+++ src/MaxSizeDict.elm
@@ -1,6 +1,7 @@
 module MaxSizeDict
     exposing
-        ( empty
+        ( MaxSizeDict
+        , empty
         , get
         , insert
         , isEmpty
@@ -11,109 +12,159 @@ module MaxSizeDict
         , size
         )
 
+{-| A dictionary with a maximum size. Items are removed from the dictionary on a
+FIFO basis when the dictionary reaches its maximum size.
+
+
+# Dictionaries
+
+@docs MaxSizeDict
+
+
+# Build
+
+@docs empty, singleton, insert, remove
+
+
+# Query
+
+@docs isEmpty, member, get, size, maxSize
+
+-}
+
 import Dict exposing (Dict)
 import Fifo exposing (Fifo)
 
 
-type alias MaxSizeDict k v =
-    { dict : Dict k v
-    , maxSize : Int
-    , keys : Fifo k
-    }
+{-| A dictionary with a maximum size.
+-}
+type MaxSizeDict k v
+    = MaxSizeDict
+        { dict : Dict k v
+        , maxSize : Int
+        , keys : Fifo k
+        }
 
 
 
 -- Build
 
 
+{-| Create an empty dictionary.
+-}
 empty : Int -> MaxSizeDict k v
 empty maxSize =
-    { dict = Dict.empty
-    , maxSize = maxSize
-    , keys = Fifo.empty
-    }
+    MaxSizeDict
+        { dict = Dict.empty
+        , maxSize = maxSize
+        , keys = Fifo.empty
+        }
 
 
+{-| Create a singleton dictionary.
+-}
 singleton : Int -> comparable -> v -> MaxSizeDict comparable v
 singleton maxSize key value =
-    { dict = Dict.singleton key value
-    , maxSize = maxSize
-    , keys = Fifo.insert key Fifo.empty
-    }
+    MaxSizeDict
+        { dict = Dict.singleton key value
+        , maxSize = maxSize
+        , keys = Fifo.insert key Fifo.empty
+        }
 
 
+{-| Insert a key-value pair into a dictionary. Replaces value when there is
+a collision.
+-}
 insert :
     comparable
     -> v
     -> MaxSizeDict comparable v
     -> MaxSizeDict comparable v
-insert key value dict =
-    if member key dict then
+insert key value ((MaxSizeDict dict) as maxSizeDict) =
+    if member key maxSizeDict then
         let
             keys =
                 removeKey key dict.keys |> Fifo.insert key
         in
-        { dict
-            | dict = Dict.insert key value dict.dict
-            , keys = keys
-        }
-    else if size dict == maxSize dict then
+        MaxSizeDict
+            { dict
+                | dict = Dict.insert key value dict.dict
+                , keys = keys
+            }
+    else if size maxSizeDict == maxSize maxSizeDict then
         case Fifo.remove dict.keys of
             -- should NOT happen
             ( Nothing, _ ) ->
-                dict
+                maxSizeDict
 
             ( Just oldest, keys ) ->
-                { dict
-                    | dict =
-                        Dict.remove oldest dict.dict
-                            |> Dict.insert key value
-                    , keys = Fifo.insert key keys
-                }
+                MaxSizeDict
+                    { dict
+                        | dict =
+                            Dict.remove oldest dict.dict
+                                |> Dict.insert key value
+                        , keys = Fifo.insert key keys
+                    }
     else
-        { dict
-            | dict = Dict.insert key value dict.dict
-            , keys = Fifo.insert key dict.keys
-        }
+        MaxSizeDict
+            { dict
+                | dict = Dict.insert key value dict.dict
+                , keys = Fifo.insert key dict.keys
+            }
 
 
+{-| Remove a key-value pair from a dictionary. If the key is not found,
+no changes are made.
+-}
 remove : comparable -> MaxSizeDict comparable v -> MaxSizeDict comparable v
-remove key dict =
-    if member key dict then
-        { dict
-            | dict = Dict.remove key dict.dict
-            , keys = removeKey key dict.keys
-        }
+remove key ((MaxSizeDict dict) as maxSizeDict) =
+    if member key maxSizeDict then
+        MaxSizeDict
+            { dict
+                | dict = Dict.remove key dict.dict
+                , keys = removeKey key dict.keys
+            }
     else
-        dict
+        maxSizeDict
 
 
 
 -- Query
 
 
+{-| Determine if a dictionary is empty.
+-}
 isEmpty : MaxSizeDict k v -> Bool
-isEmpty dict =
+isEmpty (MaxSizeDict dict) =
     Dict.isEmpty dict.dict
 
 
+{-| Determine the maximum number of key-value pairs in a dictionary.
+-}
 maxSize : MaxSizeDict k v -> Int
-maxSize =
-    .maxSize
+maxSize (MaxSizeDict dict) =
+    dict.maxSize
 
 
+{-| Determine the number of key-value pairs in a dictionary.
+-}
 size : MaxSizeDict k v -> Int
-size dict =
+size (MaxSizeDict dict) =
     Dict.size dict.dict
 
 
+{-| Determine if a key is in a dictionary.
+-}
 member : comparable -> MaxSizeDict comparable v -> Bool
-member key dict =
+member key (MaxSizeDict dict) =
     Dict.member key dict.dict
 
 
+{-| Get the value associated with a key. If the key is not found, return
+`Nothing`.
+-}
 get : comparable -> MaxSizeDict comparable v -> Maybe v
-get key dict =
+get key (MaxSizeDict dict) =
     Dict.get key dict.dict
 
 
